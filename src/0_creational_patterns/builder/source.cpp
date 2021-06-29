74ae9c611cc209c6a350efc32073fd282fad05fe
Gemuele Aludino <g.aludino@gmail.com>
2021-06-28 20:30:50 -0400

    Update source.cpp

--- src/0_creational_patterns/builder/source.cpp
+++ src/0_creational_patterns/builder/source.cpp
@@ -9,30 +9,6 @@
 #include "header.hpp"
 #include <iostream>
 
-Maze *MazeGame::CreateMaze() {
-    Maze *aMaze = new Maze;
-
-    Room *r1 = new Room(1);
-    Room *r2 = new Room(2);
-
-    Door *theDoor = new Door(r1, r2);
-
-    aMaze->AddRoom(r1);
-    aMaze->AddRoom(r2);
-
-    r1->SetSide(North, new Wall);
-    r1->SetSide(East, theDoor);
-    r1->SetSide(South, new Wall);
-    r1->SetSide(West, new Wall);
-
-    r2->SetSide(North, new Wall);
-    r2->SetSide(East, new Wall);
-    r2->SetSide(South, new Wall);
-    r2->SetSide(West, theDoor);
-
-    return aMaze;
-}
-
 Maze *MazeGame::CreateMaze(MazeBuilder &builder) {
     builder.BuildMaze();
     builder.BuildRoom(1);
@@ -69,6 +45,7 @@ void StandardMazeBuilder::BuildDoor(int n1, int n2) {
 }
 
 Direction StandardMazeBuilder::CommonWall(Room *r0, Room *r1) {
+    // TODO implementation
     return Direction::North;
 }
 
