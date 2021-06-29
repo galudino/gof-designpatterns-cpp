b1aefd112872b1bc3c493cb673b1dbb77ec28ccd
Gemuele Aludino <g.aludino@gmail.com>
2021-06-29 01:09:41 -0400

    Add sources for Adapter from text

--- src/1_structural_patterns/adapter/source.cpp
+++ src/1_structural_patterns/adapter/source.cpp
@@ -6,8 +6,37 @@
     \date
  */
 
-#include <iostream>
+#include "header.hpp"
 
-void say_hello() {
-    std::cout << "Hello, world!" << std::endl;
+void TextShape::BoundingBox(Point &bottomLeft, Point &topRight) const {
+    Coord bottom, left, width, height;
+    GetOrigin(bottom, left);
+    GetExtent(width, height);
+    bottomLeft = Point(bottom, left);
+    topRight = Point(bottom + height, left + width);
 }
+
+bool TextShape::IsEmpty() const { return TextView::IsEmpty(); }
+
+Manipulator *TextShape::CreateManipulator() const {
+    return new TextManipulator(this);
+}
+
+// object adapter
+/*
+TextShape::TextShape(TextView *t) { _text = t; }
+
+void TextShape::BoundingBox(Point &bottomLeft, Point &topRight) const {
+    Coord bottom, left, width, height;
+    _text->GetOrigin(bottom, left);
+    _text->GetExtent(width, height);
+    bottomLeft = Point(bottom, left);
+    topRight = Point(bottom + height, left + width);
+}
+
+bool TextShape::IsEmpty() const { return _text->IsEmpty(); }
+
+Manipulator *TextShape::CreateManipulator() const {
+    return new TextManipulator(this);
+}
+*/
